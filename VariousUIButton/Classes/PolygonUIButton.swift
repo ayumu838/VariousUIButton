aaaef80e904afe89abc503e664f2fcc6466fe50e
Ayumu <ayumu839.a@gmail.com>
2017-02-27 23:37:05 +0900

    Create PolygonUIButton class

---
+++ VariousUIButton/Classes/PolygonUIButton.swift
@@ -0,0 +1,17 @@
+//
+//  PolygonUIButton.swift
+//  Pods
+//
+//  Created by 望月歩 on 2017/02/27.
+//
+//
+
+import UIKit
+
+@IBDesignable
+
+class PolygonUIButton: UIButton {
+    @IBInspectable public var buttonColor : UIColor = UIColor.gray
+    @IBInspectable public var rotation : Double = 0
+    @IBInspectable public var numberOfVertex : Int = 4
+}
